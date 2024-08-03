dc1845227cfe353ad027501b82a06ee476d945ff
Baroka Irotori <birotori@gmail.com>
2024-08-03 22:45:08 +0100

    change coachCArd presentation

--- src/CoachesList.jsx
+++ src/CoachesList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ArrowLeft, Calendar, X, CreditCard, Info } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { api } from './api';
+import CoachCard from './components/CoachCard';
 
 const KKIA_PAY = import.meta.env.VITE_KKIA_PAY_KEY;
 console.log(KKIA_PAY)
@@ -72,8 +73,6 @@ const CoachesList = () => {
         });
     };
 
-    console.log({ selectedCoach })
-
     const closeModal = () => {
         setShowModal(false);
         setPaymentSuccess(false);
@@ -106,33 +105,12 @@ const CoachesList = () => {
             <h1 className="text-3xl font-bold mb-6 text-center text-fuschia-600">Nos Coachs en Orientation</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {coachesList.map(coach => (
-                    <div key={coach.id} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col h-full">
-                        <div className="h-48 sm:h-64 bg-gray-200">
-                            <img src={coach.photo_url} alt={coach.name} className="w-full h-full object-cover" />
-                        </div>
-                        <div className="p-4 flex flex-col flex-grow">
-                            <h2 className="text-xl font-semibold mb-2 text-violet-600">{coach.name}</h2>
-                            <p className="text-gray-600 text-sm mb-1">{coach.diplomes}</p>
-                            <p className="text-gray-600 text-sm mb-2 line-clamp-2">{coach.experience}</p>
-                            <p className="text-fuschia-600 font-semibold mb-2">Prix: {coach.price} FCFA</p>
-                            <div className="mt-auto pt-4 space-y-2">
-                                <button
-                                    onClick={() => handleAppointmentRequest(coach)}
-                                    className="w-full bg-gradient-to-r from-fuschia-500 to-violet-500 text-white font-bold py-2 px-4 rounded hover:from-fuschia-600 hover:to-violet-600 flex items-center justify-center"
-                                >
-                                    <Calendar size={20} className="mr-2" />
-                                    <span className="whitespace-nowrap overflow-hidden text-ellipsis">Demande de rendez-vous</span>
-                                </button>
-                                <button
-                                    onClick={() => handleShowDetails(coach)}
-                                    className="w-full bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded hover:bg-gray-300 flex items-center justify-center"
-                                >
-                                    <Info size={20} className="mr-2" />
-                                    <span className="whitespace-nowrap overflow-hidden text-ellipsis">Plus d'infos</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    <CoachCard
+                        key={coach.id}
+                        coach={coach}
+                        handleAppointmentRequest={handleAppointmentRequest}
+                        handleShowDetails={handleShowDetails}
+                    />
                 ))}
             </div>
 
